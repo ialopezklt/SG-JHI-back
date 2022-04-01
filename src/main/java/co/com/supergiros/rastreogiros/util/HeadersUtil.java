17b19776e603bcd993b0e493ec2dffd2a1b63ca3
Ivan Lopez <ivan.lopez@kalettre.com>
2022-04-01 12:57:29 -0500

    se actualiza el llamado a headers

--- src/main/java/co/com/supergiros/rastreogiros/util/HeadersUtil.java
+++ src/main/java/co/com/supergiros/rastreogiros/util/HeadersUtil.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -29,11 +28,7 @@ public final class HeadersUtil {
     public static HttpHeaders createAlert(String applicationName, String message, String param) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-" + applicationName + "-alert", message);
-        try {
-            headers.add("X-" + applicationName + "-params", URLEncoder.encode(param, StandardCharsets.UTF_8.toString()));
-        } catch (UnsupportedEncodingException e) {
-            // StandardCharsets are supported by every Java implementation so this exception will never happen
-        }
+        headers.add("X-" + applicationName + "-params", URLEncoder.encode(param, StandardCharsets.UTF_8));
         return headers;
     }
 
