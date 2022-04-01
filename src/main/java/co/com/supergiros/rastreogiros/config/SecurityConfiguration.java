bcfb509e6c9cee05c1ef5eee2b0f7975ec477935
Ivan Lopez <ivan.lopez@kalettre.com>
2022-03-31 20:56:32 -0500

    se pasan a publico la consulta de puntos de atencion

--- src/main/java/co/com/supergiros/rastreogiros/config/SecurityConfiguration.java
+++ src/main/java/co/com/supergiros/rastreogiros/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -138,6 +139,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/publico/**", "/api/authenticate", "/api/authenticatepub").permitAll()
             .antMatchers("/api/consultagiro/**", "/api/usuario/**").authenticated()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN_TI)
+            .antMatchers(HttpMethod.GET,"/api/punto-atencion").permitAll()
+            .antMatchers(HttpMethod.GET,"/api/punto-atencion/**").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/health/**").permitAll()
